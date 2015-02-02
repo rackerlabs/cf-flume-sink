44515e6935bd41a67284feec3822c229245d24a9
Damien Johnson <damien.johnson@rackspace.com>
2015-02-02 15:43:37 -0600

    Token caching

--- src/main/scala/org/openrepose/flume/sinks/KeystoneV2Connector.scala
+++ src/main/scala/org/openrepose/flume/sinks/KeystoneV2Connector.scala
@@ -1,5 +1,8 @@
 package org.openrepose.flume.sinks
 
+import java.io.{DataInputStream, InputStream}
+import java.util.Date
+
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.{ContentType, StringEntity}
 import org.apache.http.impl.client.HttpClients
@@ -16,14 +19,30 @@ import scala.util.{Success, Try}
  */
 object KeystoneV2Connector {
   private final val TOKENS_ENDPOINT = "/v2.0/tokens"
+
+  // note: Reads/writes to this static field are not atomic. As a result, multiple requests may be made to the Identity
+  //       service when this field has expired. An AtomicReference may be used to enforce stricter threading policies.
+  private var cachedToken: Option[Token] = None
 }
 
 class KeystoneV2Connector(identityHost: String) {
 
   import org.openrepose.flume.sinks.KeystoneV2Connector._
 
-  // note: caching should be done by the caller of generateToken
-  def generateToken(username: String, password: String): Try[String] = {
+  def getToken(username: String, password: String): Try[String] = {
+    cachedToken match {
+      case Some(Token(id, expiration)) if expiration.after(new Date()) =>
+        Success(id)
+      case _ =>
+        Try(requestIdentityToken(username, password))
+    }
+  }
+
+  def invalidateCachedToken(): Unit = {
+    cachedToken = None
+  }
+
+  private def requestIdentityToken(username: String, password: String): String = {
     val httpClient = HttpClients.createDefault()
     val httpPost = new HttpPost(s"$identityHost$TOKENS_ENDPOINT")
     val requestBody = compact(render(
@@ -39,17 +58,35 @@ class KeystoneV2Connector(identityHost: String) {
     try {
       httpResponse.getStatusLine.getStatusCode match {
         case HttpStatus.SC_OK | HttpStatus.SC_NON_AUTHORITATIVE_INFORMATION =>
-          implicit lazy val jsonFormats = org.json4s.DefaultFormats
           val responseEntity = httpResponse.getEntity
-          val tokenId = (parse(responseEntity.getContent) \ "access" \ "token" \ "id").extract[String]
-          EntityUtils.consume(responseEntity)
-          Success(tokenId)
-        case HttpStatus.SC_BAD_REQUEST => ???
-        case HttpStatus.SC_NOT_FOUND => ???
-        case _ => ???
+
+          if (ContentType.APPLICATION_JSON.getMimeType.equalsIgnoreCase(responseEntity.getContentType.getValue)) {
+            cachedToken = Some(parseTokenFromJson(responseEntity.getContent, responseEntity.getContentLength))
+            cachedToken.get.id
+          } else {
+            throw new Exception("Response from the identity service was not in JSON format as expected")
+          }
+        case _ =>
+          throw new Exception("Could not retrieve a token from the identity service")
       }
     } finally {
+      EntityUtils.consume(httpResponse.getEntity)
       httpResponse.close()
     }
   }
+
+  private def parseTokenFromJson(tokenStream: InputStream, contentLength: Long): Token = {
+    implicit lazy val jsonFormats = org.json4s.DefaultFormats
+
+    val contentBuffer = new Array[Byte](contentLength.toInt) // note: unsafe conversion for large values of contentLength
+    new DataInputStream(tokenStream).readFully(contentBuffer)
+    val contentString = new String(contentBuffer)
+
+    val tokenId = (parse(contentString) \ "access" \ "token" \ "id").extract[String]
+    val expirationTime = (parse(contentString) \ "access" \ "token" \ "expires").extract[Date]
+
+    Token(tokenId, expirationTime)
+  }
 }
+
+case class Token(id: String, expiration: Date)
--- src/test/scala/org/openrepose/flume/sinks/KeystoneV2ConnectorTest.scala
+++ src/test/scala/org/openrepose/flume/sinks/KeystoneV2ConnectorTest.scala
@@ -1,5 +1,8 @@
 package org.openrepose.flume.sinks
 
+import java.io.{DataInputStream, InputStream}
+import java.text.SimpleDateFormat
+import java.util.Calendar
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 
 import org.apache.http.HttpStatus
@@ -10,7 +13,7 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfterAllConfigMap, ConfigMap, FunSpec, Matchers}
 
-import scala.util.Success
+import scala.util.{Failure, Success}
 
 @RunWith(classOf[JUnitRunner])
 class KeystoneV2ConnectorTest extends FunSpec with BeforeAndAfterAllConfigMap with Matchers {
@@ -27,11 +30,35 @@ class KeystoneV2ConnectorTest extends FunSpec with BeforeAndAfterAllConfigMap wi
   }
 
   describe("generateToken") {
+    it("should send an invalid payload and handle a 4xx response") {
+      keystoneV2Connector.invalidateCachedToken()
+      val token = keystoneV2Connector.getToken("failtest", "")
+
+      token shouldBe a[Failure[_]]
+    }
     it("should send a valid payload and receive a valid token for the user provided") {
-      val token = keystoneV2Connector.generateToken("usr", "pwd")
+      keystoneV2Connector.invalidateCachedToken()
+      val token = keystoneV2Connector.getToken("usr", "pwd")
 
       token shouldBe a[Success[_]]
       token.get should equal("tkn-id")
+    }
+    it("should cache a token until invalidated") {
+      val goodToken = keystoneV2Connector.getToken("usr", "pwd")
+
+      goodToken shouldBe a[Success[_]]
+      goodToken.get should equal("tkn-id")
+
+      val badToken = keystoneV2Connector.getToken("failtest", "")
+
+      badToken shouldBe a[Success[_]]
+      badToken.get should equal("tkn-id")
+
+      keystoneV2Connector.invalidateCachedToken()
+
+      val worstToken = keystoneV2Connector.getToken("failtest", "")
+
+      worstToken shouldBe a[Failure[_]]
     }
   }
 
@@ -44,10 +71,26 @@ class KeystoneV2ConnectorTest extends FunSpec with BeforeAndAfterAllConfigMap wi
                         baseRequest: Request,
                         request: HttpServletRequest,
                         response: HttpServletResponse): Unit = {
-      response.setContentType(ContentType.APPLICATION_JSON.getMimeType)
-      response.setStatus(HttpStatus.SC_OK)
-      baseRequest.setHandled(true)
-      response.getWriter.print("{\"access\":{\"token\":{\"id\":\"tkn-id\"}}}")
+      val requestBody = readBody(request.getInputStream, request.getContentLength.toLong)
+      if (requestBody.contains("failtest")) {
+        response.sendError(400)
+      } else {
+        val expirationDate = {
+          val cal = Calendar.getInstance()
+          cal.add(Calendar.DATE, 1)
+          new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(cal.getTime)
+        }
+        response.setContentType(ContentType.APPLICATION_JSON.getMimeType)
+        response.setStatus(HttpStatus.SC_OK)
+        baseRequest.setHandled(true)
+        response.getWriter.print( s"""{"access":{"token":{"expires":"$expirationDate","id":"tkn-id"}}}""")
+      }
+    }
+
+    private def readBody(contentStream: InputStream, contentLength: Long) = {
+      val contentBuffer = new Array[Byte](contentLength.toInt) // note: unsafe conversion for large values of contentLength
+      new DataInputStream(contentStream).readFully(contentBuffer)
+      new String(contentBuffer)
     }
   }
 
