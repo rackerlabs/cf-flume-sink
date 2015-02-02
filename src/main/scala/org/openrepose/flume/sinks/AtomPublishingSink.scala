2b9f0a22bba184574b89daa808a71bd7c8a4285e
Damien Johnson <damien.johnson@rackspace.com>
2015-02-02 16:16:49 -0600

    Example structure for sink

--- src/main/scala/org/openrepose/flume/sinks/AtomPublishingSink.scala
+++ src/main/scala/org/openrepose/flume/sinks/AtomPublishingSink.scala
@@ -11,5 +11,40 @@ import org.apache.flume.sink.AbstractSink
 class AtomPublishingSink extends AbstractSink with Configurable {
   override def configure(context: Context): Unit = ???
 
-  override def process(): Status = ???
+  override def process(): Status = {
+    var status: Status = null
+
+    // Start transaction
+    val channel = getChannel
+    val txn = channel.getTransaction
+    txn.begin()
+    try {
+      // This try clause includes whatever Channel operations you want to do
+
+      val event = channel.take()
+
+      // Send the Event to the external repository.
+      // storeSomeData(e)
+
+      txn.commit()
+      status = Status.READY
+    } catch {
+      case t: Throwable =>
+        txn.rollback()
+
+        // Log exception, handle individual exceptions as needed
+
+        status = Status.BACKOFF
+
+        // re-throw all Errors
+        t match {
+          case error: Error =>
+            throw error
+          case _ =>
+        }
+    } finally {
+      txn.close()
+    }
+    status
+  }
 }
