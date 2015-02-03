e049920dce12c60ce4403048b2569dc81aa9f929
Damien Johnson <damien.johnson@rackspace.com>
2015-02-03 17:29:08 -0600

    Updated Keystone connector test to call no-argument method

--- src/test/scala/org/openrepose/flume/sinks/KeystoneV2ConnectorTest.scala
+++ src/test/scala/org/openrepose/flume/sinks/KeystoneV2ConnectorTest.scala
@@ -31,26 +31,26 @@ class KeystoneV2ConnectorTest extends FunSpec with BeforeAndAfterAllConfigMap wi
   describe("generateToken") {
     it("should send an invalid payload and handle a 4xx response") {
       val keystoneV2Connector = new KeystoneV2Connector(s"http://localhost:$localPort", "failtest", "", Map())
-      val token = keystoneV2Connector.getToken()
+      val token = keystoneV2Connector.getToken
 
       token shouldBe a[Failure[_]]
     }
     it("should send a valid payload and receive a valid token for the user provided") {
       val keystoneV2Connector = new KeystoneV2Connector(s"http://localhost:$localPort", "usr", "pwd", Map())
-      val token = keystoneV2Connector.getToken()
+      val token = keystoneV2Connector.getToken
 
       token shouldBe a[Success[_]]
       token.get should equal("tkn-id")
     }
     it("should cache a token until invalidated") {
       val keystoneV2Connector = new KeystoneV2Connector(s"http://localhost:$localPort", "usr", "pwd", Map())
-      val goodToken = keystoneV2Connector.getToken()
+      val goodToken = keystoneV2Connector.getToken
 
       goodToken shouldBe a[Success[_]]
       goodToken.get should equal("tkn-id")
       keystoneHandler.numberOfInteractions should equal(1)
 
-      val sameToken = keystoneV2Connector.getToken()
+      val sameToken = keystoneV2Connector.getToken
 
       sameToken shouldBe a[Success[_]]
       sameToken.get should equal("tkn-id")
@@ -58,7 +58,7 @@ class KeystoneV2ConnectorTest extends FunSpec with BeforeAndAfterAllConfigMap wi
 
       KeystoneV2Connector.invalidateCachedToken()
 
-      val newToken = keystoneV2Connector.getToken()
+      val newToken = keystoneV2Connector.getToken
 
       newToken shouldBe a[Success[_]]
       newToken.get should equal("tkn-id")
