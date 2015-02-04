614021a3d97ae2e9a48f694cf12078b81ea5bf61
Damien Johnson <damien.johnson@rackspace.com>
2015-02-04 10:04:52 -0600

    Pattern matching in the text to avoid mismatch due to time

--- src/test/scala/org/openrepose/flume/sinks/AtomPublisherTest.scala
+++ src/test/scala/org/openrepose/flume/sinks/AtomPublisherTest.scala
@@ -11,7 +11,7 @@ class AtomPublisherTest extends FunSpec with Matchers {
     it("wraps some content string in an Atom envelope") {
       val atomContent = AtomPublisher.pack("""{"key":"value"}""")
 
-      atomContent should equal("""<entry xmlns="http://www.w3.org/2005/Atom"><id>tag:example.org,2007:/foo/entries/1</id><title type="text">User Access Event</title><summary type="html">&lt;p>A user access event recorded by Repose.&lt;/p></summary><author><name>Repose</name></author><updated>2015-02-03T23:33:28.624Z</updated><published>2015-02-03T23:33:28.624Z</published><content type="text">{"key":"value"}</content></entry>""")
+      atomContent should fullyMatch regex """<entry xmlns="http://www.w3.org/2005/Atom"><id>tag:example.org,2007:/foo/entries/1</id><title type="text">User Access Event</title><author><name>Repose</name></author><updated>\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d.\d\d\dZ</updated><published>\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d.\d\d\dZ</published><content type="text">\{"key":"value"\}</content></entry>""".r
     }
   }
 }
