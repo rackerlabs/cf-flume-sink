7c4d4f22dc8c500ee458814d15b4cb382027c3c1
Damien Johnson <damien.johnson@rackspace.com>
2015-02-06 10:38:39 -0600

    Began working on the sink and tests for it

--- src/main/scala/org/openrepose/flume/sinks/AtomPublishingSink.scala
+++ src/main/scala/org/openrepose/flume/sinks/AtomPublishingSink.scala
@@ -1,60 +1,65 @@
 package org.openrepose.flume.sinks
 
+import com.typesafe.scalalogging.LazyLogging
 import org.apache.flume.Context
 import org.apache.flume.Sink.Status
 import org.apache.flume.conf.Configurable
 import org.apache.flume.sink.AbstractSink
 
 import scala.collection.JavaConverters._
+import scala.util.{Failure, Success}
 
 /**
  * A custom Flume sink for publishing to an endpoint using the AtomPub protocol.
  */
-class AtomPublishingSink extends AbstractSink with Configurable {
+class AtomPublishingSink extends AbstractSink with Configurable with LazyLogging {
 
   var keystoneV2Connector: KeystoneV2Connector = _
+  var feedPublisher: CloudFeedPublisher = _
 
   override def configure(context: Context): Unit = {
     keystoneV2Connector = new KeystoneV2Connector(context.getString("identity.endpoint"),
                                                   context.getString("identity.username"),
                                                   context.getString("identity.password"),
                                                   context.getSubProperties("identity.http.properties.").asScala.toMap)
+    feedPublisher = new CloudFeedPublisher(context.getString("feeds.endpoint"),
+                                           context.getSubProperties("feeds.http.properties.").asScala.toMap)
   }
 
   override def process(): Status = {
-    var status: Status = null
-
-    // Start transaction
     val channel = getChannel
     val txn = channel.getTransaction
     txn.begin()
     try {
-      // This try clause includes whatever Channel operations you want to do
-
       val event = channel.take()
 
-      // Send the Event to the external repository.
-      // storeSomeData(e)
+      val userToken = keystoneV2Connector.getToken match {
+        case Success(token) => token
+        case Failure(e) => throw e
+      }
+
+      feedPublisher.publish(new String(event.getBody), userToken)
 
       txn.commit()
-      status = Status.READY
+      Status.READY
     } catch {
       case t: Throwable =>
         txn.rollback()
 
-        // Log exception, handle individual exceptions as needed
-
-        status = Status.BACKOFF
-
-        // re-throw all Errors
         t match {
-          case error: Error =>
-            throw error
+          case er: Error =>
+            logger.error("An error occurred, event could not be processed", er)
+            throw er
+          case ue: UnauthorizedException =>
+            keystoneV2Connector.invalidateCachedToken()
+            logger.warn(s"Event could not be processed at this time: ${ue.getMessage}")
           case _ =>
+            logger.warn(s"Event could not be processed at this time: ${t.getMessage}")
         }
+
+        Status.BACKOFF
     } finally {
       txn.close()
     }
-    status
   }
 }
--- src/test/scala/org/openrepose/flume/sinks/AtomPublishingSinkTest.scala
+++ src/test/scala/org/openrepose/flume/sinks/AtomPublishingSinkTest.scala
@@ -1,11 +1,46 @@
 package org.openrepose.flume.sinks
 
+import java.nio.charset.StandardCharsets
+
+import org.apache.flume.Sink.Status
+import org.apache.flume.{Channel, Event, Transaction}
 import org.junit.runner.RunWith
+import org.mockito.Matchers.{eq => mockitoEq}
+import org.mockito.Mockito._
 import org.scalatest.junit.JUnitRunner
+import org.scalatest.mock.MockitoSugar
 import org.scalatest.{FunSpec, Matchers}
 
-@RunWith(classOf[JUnitRunner])
-class AtomPublishingSinkTest extends FunSpec with Matchers {
+import scala.util.Try
 
-  describe("process")(pending)
+@RunWith(classOf[JUnitRunner])
+class AtomPublishingSinkTest extends FunSpec with Matchers with MockitoSugar {
+
+  describe("process") {
+    it("should commit a successful transaction") {
+      val mockChannel = mock[Channel]
+      val mockTransaction = mock[Transaction]
+      val mockEvent = mock[Event]
+      val mockKeystoneConnector = mock[KeystoneV2Connector]
+      val mockFeedPublisher = mock[CloudFeedPublisher]
+      when(mockKeystoneConnector.getToken).thenReturn(Try("tkn"))
+      when(mockChannel.getTransaction).thenReturn(mockTransaction)
+      when(mockChannel.take).thenReturn(mockEvent)
+      when(mockEvent.getBody).thenReturn("tst bdy".getBytes(StandardCharsets.UTF_8))
+
+      val sink = new AtomPublishingSink()
+      sink.setChannel(mockChannel)
+      sink.keystoneV2Connector = mockKeystoneConnector
+      sink.feedPublisher = mockFeedPublisher
+
+      val status = sink.process()
+
+      status should be theSameInstanceAs Status.READY
+      verify(mockFeedPublisher, times(1)).publish(mockitoEq("tst bdy"), mockitoEq("tkn"))
+      verify(mockTransaction, times(1)).begin()
+      verify(mockTransaction, times(1)).commit()
+      verify(mockTransaction, times(1)).close()
+    }
+    it("should rollback an unsuccessful transaction")(pending)
+  }
 }
