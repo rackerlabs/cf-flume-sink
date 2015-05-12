dff4453c0dbf8fb6f5b5b36d4610803bcdc3e7b5
Damien Johnson <damien.johnson@rackspace.com>
2015-05-12 15:05:42 -0500

    Correcting the user check to check for flume not repose

--- preInstall.sh
+++ preInstall.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 getent group flume >> /dev/null 2>&1 || groupadd -r flume
-getent passwd repose >> /dev/null 2>&1 || useradd -r -g flume -s /sbin/nologin -d /usr/share/flume -c "Flume" flume
+getent passwd flume >> /dev/null 2>&1 || useradd -r -g flume -s /sbin/nologin -d /usr/share/flume -c "Flume" flume
